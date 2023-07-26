da363541a7f362a5e8cb3201ed5897369e38e29b
Bliss <blissmaake140@gmail.com>
2023-07-27 01:04:46 +0300

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,49 @@
-import React from 'react'
-import About from './Pages/About'
-import Election from './Pages/Election'
-import Header from './Pages/Header'
-import Login from './Pages/Login'
-import Profile from './Pages/Profile'
-import Signup from './Pages/Signup'
-import Voting from './Pages/Voting'
+import React from "react"
+
+// React Router Import
+import { Route, Switch, Redirect } from "react-router-dom"
+
+// Pages Import
+import About from "./Pages/About"
+import Election from "./Pages/Election"
+import Header from "./Pages/Header"
+import Login from "./Pages/Login"
+import Profile from "./Pages/Profile"
+import Signup from "./Pages/Signup"
+import Voting from "./Pages/Voting"
 
 function App() {
   return (
-    <>
-    <About/>
-   <Election/>
-   <Header/>
-   <Login/>
-   <Profile/>
-   <Signup/>
-   <Voting/>
-   
+    <div className="min-h-screen text-white">
+      {/* <Header /> */}
+      <div>
+        <Switch>
+          <Route path="/" exact>
+            <About />
+          </Route>
 
+          <Route path="/election" exact>
+            <Election />
+          </Route>
 
-    
-</>
+          <Route path="/login" exact>
+            <Login />
+          </Route>
 
+          <Route path="/profile" exact>
+            <Profile />
+          </Route>
+
+          <Route path="/signup" exact>
+            <Signup />
+          </Route>
+
+          <Route path="/voting" exact>
+            <Voting />
+          </Route>
+        </Switch>
+      </div>
+    </div>
   )
 }
 
