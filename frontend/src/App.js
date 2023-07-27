3df5786fa15cab61ee9e951c522e5fe8bb86ac9b
Juma <juma.musa@student.moringaschool.com>
2023-07-27 09:51:39 +0300

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,8 @@
 
 import React from 'react'
-import About from './Pages/About'
-import Election from './Pages/Election'
-import Header from './Pages/Header'
-import Login from './Pages/Login'
-import Profile from './Pages/Profile'
-import Signup from './Pages/Signup'
-import Voting from './Pages/Voting'
-
-function App() {
-  return (
-    <>
-    <Signup/>
-   <Login/>
-   <Header/>
-   <Profile/>
-   <Voting/>
-   <Election/>
-   <About/>
-  
-import React from "react"
-
-// React Router Import
 import { Route, Routes, Redirect } from "react-router-dom"
-
-// Pages Import
 import About from "./Pages/About"
 import Election from "./Pages/Election"
-import Header from "./Pages/Header"
 import Login from "./Pages/Login"
 import Profile from "./Pages/Profile"
 import Signup from "./Pages/Signup"
@@ -36,7 +11,6 @@ import Voting from "./Pages/Voting"
 function App() {
   return (
     <div className="min-h-screen text-white">
-      {/* <Header /> */}
       <div>
         <Routes>
           <Route exact path="/" element={<About />} />
